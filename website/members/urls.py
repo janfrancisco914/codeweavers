59bc946bc20275857e54a526b741950520e74432
Jan Francisco <71302364+janfrancisco914@users.noreply.github.com>
2024-10-23 17:58:59 +0000

    tyes

--- website/members/urls.py
+++ website/members/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('contact/', views.contact, name='contact'),  # Route for "contact" page
+    path('customize/', views.customize, name='customize'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
 ]
--- website/members/views.py
+++ website/members/views.py
@@ -4,3 +4,18 @@ from django.template import loader
 def members(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def index(request):
+    return render(request, 'index.html')  # Render the home page
+
+def customize(request):
+    return render(request, 'customize.html')  # Render the about page
+
+def contact(request):
+    return render(request, 'contact.html')  # Render the contact page
+
+def about(request):
+    return render(request, 'about.html') 
+
+def services(request):
+    return render(request, 'services.html') 
